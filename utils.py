23f76ab5f376d07f2821f78e6c250cf668bdf5fb
unknown <benedeklabancz@proton.me>
2025-03-13 21:08:22 +0100

    Load JSON

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -6,6 +7,12 @@ def load_yaml(filename, param_set):
     with open(filename, 'r') as f:
         all_sets = yaml.safe_load(f)
         return all_sets[param_set]
+
+def load_json(path):
+    with open(path, 'r') as f:
+        data = f.read()
+        data = json.loads(data)
+    return data
     
 def make_plot(values, title, x_label, y_label, path):
     values = np.array(values)
