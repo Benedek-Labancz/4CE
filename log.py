5ecd7d1f74365b901edea42c7fb0dabd6c7757ad
unknown <benedeklabancz@proton.me>
2025-03-20 20:43:14 +0100

    Add Log and Logger, moved functions

--- log.py
+++ log.py
@@ -3,6 +3,9 @@ import gym
 import torch
 import os
 import json
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
 
 def record_gym(env_name, agent, video_folder, name_prefix):
     env = gym.make(env_name, render_mode='rgb_array')
@@ -16,7 +19,7 @@ def record_gym(env_name, agent, video_folder, name_prefix):
         done = terminated or truncated
     env.close()
 
-def create_experiment(exp_name, spec):
+def create_experiment(exp_name, info, spec):
     '''
     Creates folders ready to welcome experiment logs.
     Saves the specification of the experiment as a JSON file.
@@ -44,21 +47,93 @@ def create_experiment(exp_name, spec):
     spec_json = json.dumps(spec, indent=4)
     with open(os.path.join(exp_iter_path, "spec.json"), "w") as f:
         f.write(spec_json)
+    with open(os.path.join(exp_iter_path, "info.txt"), "w") as f:
+        f.write(info)
     return video_path, plot_path, model_path
 
+def make_plot(values, title, x_label, y_label, path):
+    values = np.array(values)
+    plt.plot(values[:, 0], values[:, 1])
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.savefig(path)
+    plt.close()
 
-def save_model(model, save_folder, name_prefix):
-    '''
-    Saves the state_dict of a model to the specified path.
-    The model is saved as a .pt file.
-    '''
-    torch.save(model.state_dict(), os.path.join(save_folder, name_prefix + '.pt'))
+def make_scatter(values, title, x_label, y_label, path):
+    values = np.array(values)
+    plt.scatter(values[:, 0], values[:, 1])
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.savefig(path)
+    plt.close()
 
+def make_multiplot(values, title, x_label, y_label, path):
+    plt.plot([log[0] for log in values], [log[1] for log in values])
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.savefig(path)
+    plt.close()
 
-def load_model(agent, model_path):
-    '''
-    Load a model into an agent's Q-function.
-    '''
-    agent.Q.load_state_dict(torch.load(model_path))
-    return agent
+def make_histogram(values, title, x_label, y_label, path):
+    plt.hist(values)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.savefig(path)
+    plt.close()
 
+class Log:
+    def __init__(self, key, title, x_label=None, y_label=None, path=None, plot_func=lambda: False):
+        self.key = key
+        self.title = title
+        self.x_label = x_label
+        self.y_label = y_label
+        self.path = path
+        self.plot_func = plot_func
+        self.data = []
+
+    def reset(self):
+        self.data = []
+
+class Logger:
+    def __init__(self, log_path):
+        self.logs = dict()
+        self.log_path = log_path
+        sns.set_theme()
+
+    def add_log(self, key, title, x_label=None, y_label=None, path='default.png', plot_func=lambda: False):
+        if path is None:
+            path = self.log_path
+        self.logs[key] = Log(key, title, x_label, y_label, os.path.join(self.log_path, path), plot_func)
+
+    def log(self, key, values):
+        self.logs[key].data.append(values)
+
+    def log_t(self, key, t, values):
+        self.logs[key].data.append([t, values])
+
+    def clear_log(self, key):
+        self.logs[key].reset()
+
+    def plot_all(self):
+        for key in self.logs:
+            self.logs[key].plot_func(key)
+    
+    def make_line_plot(self, key):
+        log = self.logs[key]
+        make_plot(log.data, log.title, log.x_label, log.y_label, log.path)
+
+    def make_scatter(self, key):
+        log = self.logs[key]
+        make_scatter(log.data, log.title, log.x_label, log.y_label, log.path)
+
+    def make_multiline_plot(self, key):
+        log = self.logs[key]
+        make_multiplot(log.data, log.title, log.x_label, log.y_label, log.path)
+
+    def make_histogram(self, key):
+        log = self.logs[key]
+        make_histogram(log.data, log.title, log.x_label, log.y_label, log.path)
